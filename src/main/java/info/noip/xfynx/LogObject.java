d9a34f9a937dbab2f9dfca2fc5436c8604358637
xfynx <steewenson.evan@gmail.com>
2013-07-20 01:30:41 +0600

    убран костыль LogString.copy(Logstring logstring). тип списка сменил со связного на массивный.

--- src/main/java/info/noip/xfynx/LogObject.java
+++ src/main/java/info/noip/xfynx/LogObject.java
@@ -3,9 +3,9 @@ package info.noip.xfynx;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -18,18 +18,18 @@ import java.util.List;
  */
 public class LogObject {
 
-    public List<LogString> logInformation = new LinkedList<>();
-    private List<String> result = new LinkedList<>();
+    public List<LogString> logInformation = new ArrayList<>();
+    private List<String> result = new ArrayList<>();
 
     public LogObject(String fileName) {
         this.logInformation = read(fileName);
     }
 
     private List<LogString> parse(List<LogString> logInformation) {
-        LogString parsingString = new LogString();
         String[] splitLogString;
-        parsingString.gregorianCalendar = new GregorianCalendar();
         for (String s : this.result) {
+            LogString parsingString = new LogString();
+            parsingString.gregorianCalendar = new GregorianCalendar();
             splitLogString = s.split("\t");
             Date tempDate = new Date(Long.valueOf(splitLogString[0]));    //Штуки вида Long.valueOf(splitLogString[0])-
             parsingString.gregorianCalendar.setTime(tempDate);            //преобразование из элемента String-массива
@@ -37,9 +37,9 @@ public class LogObject {
             parsingString.userId = Long.valueOf(splitLogString[2]);
             parsingString.siteId = Long.valueOf(splitLogString[3]);
             parsingString.event = splitLogString[4];
-            LogString temp = new LogString();        //сей костыль пришлось применить из-за того, что в добавлении
-            temp.copy(parsingString);                //в logInformation ссылка на каждый элемент parser менялась,
-            logInformation.add(temp);                //что, в итоге, давала все элементы списка, равные последнему в файле.
+            //LogString temp = new LogString();        //сей костыль пришлось применить из-за того, что в добавлении
+            //temp.copy(parsingString);                //в logInformation ссылка на каждый элемент parser менялась,
+            logInformation.add(parsingString);                //что, в итоге, давала все элементы списка, равные последнему в файле.
         }
         return logInformation;
     }
--- src/main/java/info/noip/xfynx/LogString.java
+++ src/main/java/info/noip/xfynx/LogString.java
@@ -11,8 +11,6 @@ import java.util.GregorianCalendar;
  *      использовать List<LogString>
  */
 public class LogString {
-    //public long rawDate;
-    //public Date date;
     public GregorianCalendar gregorianCalendar;
     public long countryCode;
     public long userId;
@@ -20,8 +18,6 @@ public class LogString {
     public String event;
 
     public LogString() {
-        //this.rawDate = 0;
-        //this.date = null;
         this.gregorianCalendar = null;
         this.countryCode = 0;
         this.userId = 0;
@@ -29,22 +25,9 @@ public class LogString {
         this.event = null;
     }
 
-    protected LogString copy(LogString in) {
-        //this.rawDate = in.rawDate;
-        //this.date = in.date;
-        this.gregorianCalendar = in.gregorianCalendar;
-        this.countryCode = in.countryCode;
-        this.userId = in.userId;
-        this.siteId = in.siteId;
-        this.event = in.event;
-        return this;
-    }
-
     @Override
     public String toString() {
         return "LogString{" +
-                //"rawDate=" + rawDate + ",
-                //"date=" + ft.format(date) +
                 "date=" + gregorianCalendar.getTime() +
                 ", countryCode=" + countryCode +
                 ", userId=" + userId +
