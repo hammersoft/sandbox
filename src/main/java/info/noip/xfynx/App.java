9f1d22c3fee615446519a7f6441d0249fc1ab933
xfynx <steewenson.evan@gmail.com>
2013-07-19 12:35:26 +0600

    LogReader.java. Теперь это хозяйство может читать файлы, хотя и бестолково.

--- src/main/java/info/noip/xfynx/App.java
+++ src/main/java/info/noip/xfynx/App.java
@@ -1,9 +1,45 @@
 package info.noip.xfynx;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static void help() {
+        System.out.println("use this keys:");
+        System.out.println("-sort: sorting file names of logs");
+        System.out.println("-scCnt: statistic by shows and clicks by SiteId and Date");
+        System.out.println("-usrCnt: count of users by SiteId");
+        System.out.println("-vstCnt: count of visits by User");
+        System.out.println("-frqDct: frequency dictionary of visits by User");
+        System.out.println("-help: show this info.");
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1)
+            help();
+        for (String arg : args)
+            switch (arg) {
+                case "-sort":
+                    System.out.println("-sort: sorting file names of logs");
+                    break;
+                case "-scCnt":
+                    System.out.println("-scCnt: statistic by shows and clicks by SiteId and Date");
+                    LogReader logReader = new LogReader("log.txt");
+                    logReader.out();
+                    break;
+                case "-usrCnt":
+                    System.out.println("-usrCnt: count of users by SiteId");
+                    break;
+                case "-vstCnt":
+                    System.out.println("-vstCnt: count of visits by User");
+                    break;
+                case "-frqDct":
+                    System.out.println("-frqDct: frequency dictionary of visits by User");
+                    break;
+                case "-help":
+                    help();
+                    break;
+                default:
+                    help();
+                    break;
+            }
+        //LogReader logReader = new LogReader("d:/sandbox/log.txt");
     }
 }
