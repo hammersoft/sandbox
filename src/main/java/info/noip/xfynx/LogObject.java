6485a428ec54e98d3fd3e2b26fd18d8dd3700774
xfynx <steewenson.evan@gmail.com>
2013-07-19 16:36:57 +0600

    изменена структура классов. LogObject теперь есть объект, который содержит связный список LogString, то есть полей, которые и представляют собой значения столбцов лога. ещё все ключи сделал в lowerCase, позже добавлю автопреобразование

---
+++ src/main/java/info/noip/xfynx/LogObject.java
@@ -0,0 +1,65 @@
+package info.noip.xfynx;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author xfynx
+ */
+
+/**
+ * @see LogObject объект, который, при вызове конструктора с путём лог-файла,
+ *      автоматически парсит его в структуру List<LogString>
+ */
+public class LogObject {
+
+    public List<LogString> logInformation = new LinkedList<>();
+    private List<String> result = new LinkedList<>();
+
+    public LogObject(String fileName) {
+        this.logInformation = read(fileName);
+    }
+
+    private List<LogString> parse(List<LogString> logInformation) {
+        LogString parsingString = new LogString();
+        String[] splitLogString;
+        for (String s : this.result) {
+            splitLogString = s.split("\t");
+            parsingString.rawDate = Long.valueOf(splitLogString[0]);
+            parsingString.countryCode = Long.valueOf(splitLogString[1]);
+            parsingString.userId = Long.valueOf(splitLogString[2]);
+            parsingString.siteId = Long.valueOf(splitLogString[3]);
+            parsingString.event = splitLogString[4];
+            LogString temp = new LogString();        //сей костыль пришлось применить из-за того, что в добавлении
+            temp.copy(parsingString);                //в logInformation ссылка на каждый элемент parser менялась,
+            logInformation.add(temp);                //что, в итоге, давала все элементы списка, равные последнему в файле.
+        }
+        return logInformation;
+    }
+
+    private List<LogString> read(String fileName) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                this.result.add(s);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.printf("File %s not found.%n", fileName);
+        }
+        return parse(logInformation);
+    }
+
+    public void showParsed() {
+        int i = 0;
+        for (LogString logString : logInformation) {
+            System.out.println(logString.toString());
+            i++;
+        }
+        System.out.println(i);
+    }
+}
