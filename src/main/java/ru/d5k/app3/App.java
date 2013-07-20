2984ad1065e9713748564301f4ed437a9973db0a
dimaws <dmitry.yakubov@gmail.com>
2013-07-20 12:49:44 +0600

    попытка залить проект

---
+++ src/main/java/ru/d5k/app3/App.java
@@ -0,0 +1,14 @@
+package ru.d5k.app3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String s = "World";
+        System.out.printf( "Hello, %s !", s);
+    }
+}
